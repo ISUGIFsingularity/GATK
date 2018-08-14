44e6bb1226fd35d2f27556fd287f48aa35888118
Andrew J Severin <gif@iastate.edu>
2018-08-14 08:27:58 -0500

    GATK singularity container is ready to test

--- bin/GATK_00_PrepareRef.sh
+++ bin/GATK_00_PrepareRef.sh
@@ -12,29 +12,29 @@
 #module load bioawk
 
 #change this variable to correspond to the directory you downloaded the git repository
-export GENMODgit="/pylon5/mc48o5p/severin/isugif/genomeModules"
+export GENMODgit="/pylon5/mc48o5p/severin/isugif/GATK"
 export TMPDIR="./"
 
 REF="$1"
 export BASEREF=$(basename ${REF%.*})_sorted
 
 #index genome for (a) picard, (b) samtools and (c) bwa
-####need to change bioawk to singularity but can't update yet because bowtie2 is throwing an error on spack
-bioawk -c fastx '{print}' $REF | sort -k1,1V -T $TMPDIR | awk '{print ">"$1;print $2}' > ${BASEREF}.fa
-#parallel <<FIL
-${GENMODgit}/wrappers/GM picard CreateSequenceDictionary \
+${GENMODgit}/wrappers/GATK bioawk -c fastx '{print}' $REF | sort -k1,1V -T $TMPDIR | awk '{print ">"$1;print $2}' > ${BASEREF}.fa
+
+${GENMODgit}/wrappers/GATK parallel <<FIL
+${GENMODgit}/wrappers/GATK picard CreateSequenceDictionary \
   REFERENCE=${BASEREF}.fa \
   OUTPUT=${BASEREF}.dict
-${GENMODgit}/wrappers/GM samtools faidx ${BASEREF}.fa
-${GENMODgit}/wrappers/GM bwa index -a bwtsw ${BASEREF}.fa
-#FIL
+${GENMODgit}/wrappers/GATK samtools faidx ${BASEREF}.fa
+${GENMODgit}/wrappers/GATK bwa index -a bwtsw ${BASEREF}.fa
+FIL
 
 
 
 # Create interval list (here 100 kb intervals)
 ${GENMODgit}/wrappers/fasta_length ${BASEREF}.fa > ${BASEREF}_length.txt
-${GENMODgit}/wrappers/GM bedtools makewindows -w 100000 -g ${BASEREF}_length.txt > ${BASEREF}_100kb_coords.bed
-${GENMODgit}/wrappers/GM picard BedToIntervalList \
+${GENMODgit}/wrappers/GATK bedtools makewindows -w 100000 -g ${BASEREF}_length.txt > ${BASEREF}_100kb_coords.bed
+${GENMODgit}/wrappers/GATK picard BedToIntervalList \
   INPUT= ${BASEREF}_100kb_coords.bed \
   SEQUENCE_DICTIONARY=${BASEREF}.dict \
-  OUTPUT=${BASEREF}_100kb_gatk_intervals.list
+  OUTPUT=${BASEREF}_100kb_gatk_intervals.li st
--- bin/GATK_01_RunBWA.sh
+++ bin/GATK_01_RunBWA.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# perfomrs mapping of reads to the indexed reference genome
+# performs mapping of reads to the indexed reference genome
 # uses the options specified in "best practices"
 
 #command genomeModule READ1 READ2
 
-#module load picard/2.4.1
-#module load java
-#module load bwa/0.7.12
-#module load samtools
-#module load $1
 
-GENOMEDIR="./"
-GNAME=$1
+#change this variable to correspond to the directory you downloaded the git repository
+export GENMODgit="/pylon5/mc48o5p/severin/isugif/GATK"
 
-REF="$GENOMEDIR/$GNAME"
+
+REF="$1"  ## same as input for GATK_00
+
+# This assumes that you ran GATK_00 to sort the original reference file.
+export REF=$(basename ${REF%.*})_sorted.fa
+
+
 # this option might be the frequetly changed, hence not it's a variable
 THREADS="16"
 # if the reads are paired then use -p option
@@ -21,12 +22,12 @@ if [ "$#" -eq 3 ]; then
   READ1="$2"
   READ2="$3"
   OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
-  ${GENMODgit}/wrappers/GM bwa mem -M -t ${THREADS} ${REF} ${READ1} ${READ2} | samtools view -buS - > ${OUTNAME}.bam
+  ${GENMODgit}/wrappers/GATK bwa mem -M -t ${THREADS} ${REF} ${READ1} ${READ2} | samtools view -buS - > ${OUTNAME}.bam
 # if not just use the reads as single reads
 elif [ "$#" -eq 1 ]; then
   READ1="$2"
   OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
-  ${GENMODgit}/wrappers/GM bwa mem -M -t ${THREADS} ${REF} ${READ1} | samtools view -buS - > ${OUTNAME}.bam
+  ${GENMODgit}/wrappers/GATK bwa mem -M -t ${THREADS} ${REF} ${READ1} | samtools view -buS - > ${OUTNAME}.bam
 # if number of arguments do not match, raise error
 else
   echo "ERROR: INVALID NUMBER OF ARGUMENTS"
