ab9a13e84233b775a5484e0ff5cc3cec79751ed6
Andrew J Severin <gif@iastate.edu>
2018-08-16 14:13:54 -0500

    echo statements and fasta index

--- bin/GATK_new.sh
+++ bin/GATK_new.sh
@@ -13,7 +13,7 @@ READ2="$3"
 
 export BASEREF=$(basename ${REF%.*})
 
-### [FASTQ to uBAM](https://gatkforums.broadinstitute.org/gatk/discussion/6484#latest#top)
+echo "[FASTQ to uBAM](https://gatkforums.broadinstitute.org/gatk/discussion/6484#latest#top)""
 
 
 
@@ -22,19 +22,19 @@ ${GATKgit}/wrappers/GATK picard FastqToSam FASTQ=${READ1} FASTQ2=${READ2}  OUTPU
 
 
 
-#### Mark Illumina adapters
+echo "Mark Illumina adapters"
 
 
 ${GATKgit}/wrappers/GATK picard MarkIlluminaAdapters I=$(basename ${READ1%.*})_fastqtosam.bam O=$(basename ${READ1%.*})_markilluminaadapters.bam M=$(basename ${READ1%.*})_markilluminaadapters_metrics.txt TMP_DIR=$LOCAL
 
 
-#### 3A convert BAM to fastq
+echo "3A convert BAM to fastq"
 
 
 ${GATKgit}/wrappers/GATK picard SamToFastq I=$(basename ${READ1%.*})_markilluminaadapters.bam FASTQ=$(basename ${READ1%.*})_samtofastq_interleaved.fq CLIPPING_ATTRIBUTE=XT CLIPPING_ACTION=2 INTERLEAVE=true NON_PF=true TMP_DIR=$LOCAL
 
 
-#### 3B Align reads and flag secondary hits using BWA-MEM
+echo "3B Align reads and flag secondary hits using BWA-MEM"
 
 
 ${GATKgit}/wrappers/GATK bwa index -a bwtsw ${BASEREF}.fasta
@@ -42,26 +42,27 @@ ${GATKgit}/wrappers/GATK bwa index -a bwtsw ${BASEREF}.fasta
 ${GATKgit}/wrappers/GATK bwa mem -M -t 7 -p ${BASEREF}.fasta $(basename ${READ1%.*})_samtofastq_interleaved.fq > $(basename ${READ1%.*})_bwa_mem.sam
 
 
-#### 3C Restore altered data and apply & adjust meta information using MergeBamAlignment
+echo "3C Restore altered data and apply & adjust meta information using MergeBamAlignment"
 
 
-#need sequence dictionary
+echo "need sequence dictionary and Reference index file"
+${GATKgit}/wrappers/GATK samtools faidx ${BASEREF}.fasta
 ${GATKgit}/wrappers/GATK picard CreateSequenceDictionary \
   REFERENCE=${BASEREF}.fasta \
   OUTPUT=${BASEREF}.dict
 
-#merge BAM Alignment
+echo "merge BAM Alignment"
 ${GATKgit}/wrappers/GATK picard MergeBamAlignment R=${BASEREF}.fasta UNMAPPED_BAM=$(basename ${READ1%.*})_fastqtosam.bam ALIGNED_BAM=$(basename ${READ1%.*})_bwa_mem.sam O=$(basename ${READ1%.*})_mergebamalignment.bam CREATE_INDEX=true ADD_MATE_CIGAR=true CLIP_ADAPTERS=false CLIP_OVERLAPPING_READS=true INCLUDE_SECONDARY_ALIGNMENTS=true MAX_INSERTIONS_OR_DELETIONS=-1 PRIMARY_ALIGNMENT_STRATEGY=MostDistant ATTRIBUTES_TO_RETAIN=XS TMP_DIR=$LOCAL
 
 
 
-#### Mark Duplicates https://software.broadinstitute.org/gatk/documentation/tooldocs/current/picard_sam_markduplicates_MarkDuplicates.php
+echo "Mark Duplicates https://software.broadinstitute.org/gatk/documentation/tooldocs/current/picard_sam_markduplicates_MarkDuplicates.php"
 
 
 ${GATKgit}/wrappers/GATK picard MarkDuplicates INPUT=$(basename ${READ1%.*})_mergebamalignment.bam OUTPUT=$(basename ${READ1%.*})_mergebamalignment_markduplicates.bam METRICS_FILE=$(basename ${READ1%.*})_mergebamalignment_markduplicates_metrics.txt OPTICAL_DUPLICATE_PIXEL_DISTANCE=2500 CREATE_INDEX=true TMP_DIR=$LOCAL
 
 
-#### Call with HaplotypeCaller https://software.broadinstitute.org/gatk/documentation/tooldocs/current/org_broadinstitute_hellbender_tools_walkers_haplotypecaller_HaplotypeCaller.php
+echo "Call with HaplotypeCaller https://software.broadinstitute.org/gatk/documentation/tooldocs/current/org_broadinstitute_hellbender_tools_walkers_haplotypecaller_HaplotypeCaller.php"
 
 
- ${GATKgit}/wrappers/GATK gatk HaplotypeCaller -R ${BASEREF}.fasta -I $(basename ${READ1%.*})_mergebamalignment_markduplicates.bam --output test.vcf
+ ${GATKgit}/wrappers/GATK gatk HaplotypeCaller -R ${BASEREF}.fasta -I $(basename ${READ1%.*})_mergebamalignme nt_markduplicates.bam --output test.vcf
