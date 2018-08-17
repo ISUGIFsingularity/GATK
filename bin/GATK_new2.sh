e16722977952209b574f9a4f1b22c49f0a528648
Andrew Severin <severin@iastate.edu>
2018-08-17 13:12:43 -0400

    got it working

--- bin/GATK_new2.sh
+++ bin/GATK_new2.sh
@@ -19,10 +19,12 @@ export BASEREF=$(basename ${REF%.*})
 
 
 # generate gatk haplotype caller functions on intervals and generate slurms
-    GENOMEINTERVALS=${BASEREF}_100kb_gatk_intervals.list
+    export GENOMEINTERVALS=${BASEREF}_100kb_gatk_intervals.list
 
     # Create interval list (here 100 kb intervals)
-    ${GATKgit}/wrappers/fasta_length ${BASEREF}.fa > ${BASEREF}_length.txt
+    ${GATKgit}/wrappers/fasta_length ${BASEREF}.fasta > ${BASEREF}_length.txt
+    # Not sure where the pwd line at the top of the file is coming from but deleted it.
+    sed -i -e "1d" ${BASEREF}_length.txt
     ${GATKgit}/wrappers/GATK bedtools makewindows -w 100000 -g ${BASEREF}_length.txt > ${BASEREF}_100kb_coords.bed
     ${GATKgit}/wrappers/GATK picard BedToIntervalList \
       INPUT= ${BASEREF}_100kb_coords.bed \
@@ -39,16 +41,21 @@ export BASEREF=$(basename ${REF%.*})
 
     #combine the reference genome, 100k genomic intervals and the input files into gatk commands
     #need to figure out how to include direct path to genomeanalysistk.jar file if that is necessary
-    while read line; do \
-    g2=$(echo $line | awk '{print $1":"$2"-"$3}'); \
-    g1=$(echo $line | awk '{print $1"_"$2"_"$3}'); \
-    CWD=$(pwd)
-    echo -n "${GATKgit}/wrappers/GATK gatk HaplotypeCaller  \
-    -R ${GENOMEFASTA} \
-    $(cat temp) \
-    -L "${g2}" --output \${TMPDIR}/"${g1}".vcf;"; \
-    echo "mv \${TMPDIR}/"${g1}".vcf $CWD" ; \
-    done< $(grep -v "@" ${GENOMEINTERVALS})  > gatk.cmds
+    
+    echo $GENOMEINTERVALS
+while read line; do \
+if ! [[ $line == @* ]]; then \
+g2=$(echo $line | awk '{print $1":"$2"-"$3}'); \
+g1=$(echo $line | awk '{print $1"_"$2"_"$3}'); \
+CWD=$(pwd)
+echo -n "${GATKgit}/wrappers/GATK gatk HaplotypeCaller  \
+-R ${GENOMEFASTA} \
+$(cat temp) \
+-L "${g2}" --output \${TMPDIR}/"${g1}".vcf;"; \
+echo "mv \${TMPDIR}/"${g1}".vcf $CWD" ; \
+fi           
+done<${GENOMEINTERVALS} > gatk.cmds
+
 
 
 
