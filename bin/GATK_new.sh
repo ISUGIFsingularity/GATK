0160149631bcee6dbceb535b037542c50cc093e9
Andrew J Severin <gif@iastate.edu>
2018-08-17 12:21:01 -0500

    REf alias

--- bin/GATK_new.sh
+++ bin/GATK_new.sh
@@ -93,7 +93,7 @@
     g1=$(echo $line | awk '{print $1"_"$2"_"$3}'); \
     CWD=$(pwd)
     echo -n "${GATKgit}/wrappers/GATK gatk HaplotypeCaller  \
-    -R ${GENOMEFASTA} \
+    -R ${REF} \
     $(cat temp) \
     -L "${g2}" --output \${TMPDIR}/"${g1}".vcf;"; \
     echo "mv \${TMPDIR}/"${g1}".vcf $CWD" ; \
